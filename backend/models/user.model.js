25cef842963f608a1405470135dbb830dd7a656a
Vighnesh-code <vighneshzagade4@gmail.com>
2025-05-08 10:35:02 +0530

    added-user-model

---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required!"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Password must be atleast 6 characters long!"],
+    },
+    cartItems: {
+      quantity: {
+        type: Number,
+        deafult: 1,
+      },
+      product: {
+        type: mongoose.Types.Schema.ObjectId,
+        ref: "Product",
+      },
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
