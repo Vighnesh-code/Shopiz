3ae7a123a3ed3c256a168d54cfeed12f69b0b89d
Vighnesh-code <vighneshzagade4@gmail.com>
2025-05-08 11:45:04 +0530

    signup-test-api-created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,37 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/user.model.js";
+
 export const signup = async (req, res) => {
   try {
-    res.send("Hey from signup Page");
+    const { name, email, password } = req.body;
+    if (!email || !password || !name) {
+      return res.status(400).json({ message: "Please fill all credentials!" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already registered!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const newUser = await user.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Account Created Successfully",
+      user: newUser,
+    });
   } catch (error) {
     console.log(`Error in signup Controller: ${error.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error!" });
   }
 };
 
