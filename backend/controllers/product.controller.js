643df12cc3196fd57879e463ba06e6161682f3ac
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-09 13:42:44 +0000

    get featured products api endpoint created

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { Product } from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 export const createProduct = async (req, res) => {
   try {
@@ -43,5 +44,22 @@ export const getAllProducts = async (req, res) => {
   } catch (error) {
     console.log(`Error in getAllProducts controller: ${error.message}`);
     res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get(`featured_products`);
+    if (featuredProducts) return res.json(JSON.parse(featuredProducts));
+
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+    if (!featuredProducts)
+      return res.status(404).json({ message: "No Featured Products Found!" });
+
+    await redis.set(`featured_products`, JSON.stringify(featuredProducts));
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log(`Error in getFeaturedProducts: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createProduct);
 router.get("/", protectRoute, getAllProducts);
+router.get("/featured", protectRoute, getFeaturedProducts);
 
 export default router;
