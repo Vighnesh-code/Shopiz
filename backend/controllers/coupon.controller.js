a9a3aa7f4c16329a7715bf185602b506742be1ee
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-10 09:29:04 +0000

    added validateCoupon api endpoint

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -12,3 +12,31 @@ export const getCoupon = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error!" });
   }
 };
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const coupon = await Coupon.findOne({
+      code: code,
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    if (!coupon) return res.status(404).json({ message: "Coupon not found!" });
+
+    if (coupon.expirationDate < Date.now()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(404).json({ message: "Coupon is expired!" });
+    }
+
+    res.json({
+      message: "Coupon is Valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log(`Error in validateCoupon: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
