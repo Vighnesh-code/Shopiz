ff95325e29005240ad0efac40396ab5fee259715
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-10 05:55:33 +0000

    added coupon model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
@@ -10,7 +11,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   connectDB();
