65f55db5fa8177362fb7ad9affbeb8830a2d62ec
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-10 03:59:30 +0000

    minor fix

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,7 +2,9 @@ import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
+  getFeaturedProducts,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
@@ -10,5 +12,6 @@ const router = express.Router();
 router.post("/create", protectRoute, createProduct);
 router.get("/", protectRoute, getAllProducts);
 router.get("/featured", protectRoute, getFeaturedProducts);
+router.delete("/:id", protectRoute, deleteProduct);
 
 export default router;
