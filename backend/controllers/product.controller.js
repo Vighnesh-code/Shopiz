6ed0604e13a70f01f0cf3646196af4fe05017451
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-10 04:06:25 +0000

    added getRecommendedProducts api endpoint

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -88,3 +88,27 @@ export const deleteProduct = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getRecommededProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    console.log(`Error in getRecommededProducts: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,12 +5,14 @@ import {
   deleteProduct,
   getAllProducts,
   getFeaturedProducts,
+  getRecommededProducts,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createProduct);
 router.get("/", protectRoute, getAllProducts);
+router.get("/recommendations", protectRoute, getRecommededProducts);
 router.get("/featured", protectRoute, getFeaturedProducts);
 router.delete("/:id", protectRoute, deleteProduct);
 
