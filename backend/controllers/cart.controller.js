0c1607de0c08ac8730c21c64c256e4cf2c411725
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-11 05:59:22 +0000

    added removeAllFromCart api endpoint

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -38,3 +38,20 @@ export const getCartProducts = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error!" });
   }
 };
+
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+    }
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    console.log(`Error in removeAllFromCart: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { addToCart, getCartProducts } from "../controllers/cart.controller.js";
+import {
+  addToCart,
+  getCartProducts,
+  removeAllFromCart,
+} from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, addToCart);
 router.get("/", protectRoute, getCartProducts);
+router.delete("/", protectRoute, removeAllFromCart);
 
 export default router;
