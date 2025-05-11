4908885ad362527f9e0cff93e7c7ab2400af56db
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-11 05:20:34 +0000

    added getCartProducts api endpoint

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,5 @@
+import { Product } from "../models/product.model.js";
+
 export const addToCart = async (req, res) => {
   try {
     const { productId } = req.body;
@@ -14,6 +16,25 @@ export const addToCart = async (req, res) => {
     res.json(user.cartItems);
   } catch (error) {
     console.log(`Error in addToCart: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+export const getCartProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ _id: { $in: req.user.cartItems } });
+
+    // Add quantity for each product
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+
+    res.json(cartItems);
+  } catch (error) {
+    console.log(`Error in getCartProducts: ${error.message}`);
     res.status(500).json({ message: "Internal Server Error!" });
   }
 };
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { addToCart } from "../controllers/cart.controller.js";
+import { addToCart, getCartProducts } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, addToCart);
+router.get("/", protectRoute, getCartProducts);
 
 export default router;
