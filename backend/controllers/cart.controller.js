faf167d39dd50f6eaffe46ab8f18c91701798de0
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-05-11 09:49:08 +0000

    added update quantity api endpoint

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -55,3 +55,27 @@ export const removeAllFromCart = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error!" });
   }
 };
+
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        await user.save();
+        return res.json(user.cartItems);
+      }
+
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+    }
+  } catch (error) {
+    console.log(`Error in updateQuantity: ${error.message}`);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -4,6 +4,7 @@ import {
   addToCart,
   getCartProducts,
   removeAllFromCart,
+  updateQuantity,
 } from "../controllers/cart.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", protectRoute, addToCart);
 router.get("/", protectRoute, getCartProducts);
 router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 export default router;
