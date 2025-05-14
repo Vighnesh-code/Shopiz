90113a20f53981689b4a7861175f3f079a8bcb8d
Vighnesh-code <vighneshzagade4@gmail.com>
2025-05-14 10:31:06 +0000

    Created Signup Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
-      <div className="flex flex-wrap justify-between items-center">
+      <div className="flex flex-wrap justify-between items-center p-4">
         <Link
           to="/"
           className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
